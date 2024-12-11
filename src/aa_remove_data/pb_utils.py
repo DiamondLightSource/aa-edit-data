c2ff4988a20d21c20c3a952f88cbbfa02de1cd12
Jacob Williamson <hvj62882@pc0132.cs.diamond.ac.uk>
2024-12-11 10:57:00 +0000

    Improve naming of variables

--- src/aa_remove_data/pb_utils.py
+++ src/aa_remove_data/pb_utils.py
@@ -8,8 +8,8 @@ class PBUtils:
 
     def __init__(self, filepath: Optional[PathLike] = None):
         self.header = EPICSEvent_pb2.PayloadInfo()
-        self.messages = []
-        self.message_type = ''
+        self.samples = []
+        self.sample_type = ''
         if filepath:
             self.read_pb(filepath)
 
@@ -29,36 +29,36 @@ class PBUtils:
 
     def _convert_to_class_name(self, enum_name: str) -> str:
         # Split the enum name by underscores and capitalize each part
-        parts = enum_name.split('_')
+        parts = sample_type.split('_')
         return ''.join(part.capitalize() for part in parts)
 
-    def get_message_type(self) -> str:
+    def get_sample_type(self) -> str:
         type_descriptor = self.header.DESCRIPTOR.fields_by_name["type"]
         enum_descriptor = type_descriptor.enum_type
         return enum_descriptor.values_by_number[self.header.type].name
 
-    def get_message_class(self) -> Type[Message]:
-        # Ensure self.message_type is set first.
-        if not self.message_type:
-            self.message_type = self.get_message_type()
-        message_type_camel = self._convert_to_class_name(self.message_type)
-        message_class = getattr(EPICSEvent_pb2, message_type_camel)
-        return message_class
+    def get_sample_class(self) -> Type[Message]:
+        # Ensure self.sample_type is set first.
+        if not self.sample_type:
+            self.sample_type = self.get_sample_type()
+        sample_type_camel = self._convert_to_class_name(self.sample_type)
+        sample_class = getattr(EPICSEvent_pb2, sample_type_camel)
+        return sample_class
 
     def read_pb(self, filepath: PathLike):
         with open(filepath, 'rb') as f:
             first_line = self._unescape_data(f.readline().strip())
             self.header.ParseFromString(first_line)
-            message_class = self.get_message_class()
+            sample_class = self.get_sample_class()
             lines = f.readlines()
-            self.messages = [message_class() for n in range(len(lines))]
-            for i, message in enumerate(self.messages):
+            self.samples = [sample_class() for n in range(len(lines))]
+            for i, sample in enumerate(self.samples):
                 line = self._unescape_data(lines[i].strip())
-                message.ParseFromString(line)
+                sample.ParseFromString(line)
 
     def write_pb(self, filepath: PathLike):
         header_b = self._escape_data(self.header.SerializeToString()) + b'\n'
-        messages_b = [self._escape_data(message.SerializeToString()) + b'\n'
-                      for message in self.messages]
+        samples_b = [self._escape_data(sample.SerializeToString()) + b'\n'
+                     for sample in self.samples]
         with open(filepath, "wb") as f:
-            f.writelines([header_b] + messages_b)
+            f.writelines([header_b] + samples_b)
