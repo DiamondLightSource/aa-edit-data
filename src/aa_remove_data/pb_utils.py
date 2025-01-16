cfda78060ffc1c644264451f92c1210987b18deb
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-01-16 11:50:45 +0000

    Improve docstrings

--- src/aa_remove_data/pb_utils.py
+++ src/aa_remove_data/pb_utils.py
@@ -64,8 +64,10 @@ class PBUtils:
         return data
 
     def _get_proto_class_name(self) -> str:
-        """Convert the name of a pv type to CamelCase to match the proto class
-        name.
+        """Convert the name of a pv type to match the proto class name. This
+        involves coverting to CamelCase and replacing 'WAVEFORM' with 'Vector'.
+        The full mapping is described in
+        epicsarchiverap/src/main/edu/stanford/slac/archiverappliance/PB/data/DBR2PBTypeMapping.java
 
         Returns:
             str: Name of proto class, e.g VectorDouble.
